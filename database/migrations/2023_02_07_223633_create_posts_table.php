d880561287458c3423d8e3f8657eeb64adbf1d83
Andrey Fits <andreyfits@gmail.com>
2023-02-10 01:43:27 +0300

    feat: create posts table

---
+++ database/migrations/2023_02_07_223633_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('slug', 100)->unique();
+            $table->text('content');
+            $table->string('featured_image');
+            $table->tinyInteger('featured_post')->default(0);
+            $table->boolean('active')->default(0);
+            $table->integer('category_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->integer('views')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
