528c46dc01f75aed7c9a33ea682d8a63ba16ba33
Andrey Fits <andreyfits@gmail.com>
2023-02-10 23:39:23 +0300

    refactor: small changes in the templates

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -6,6 +6,7 @@
 <title>{{ config('app.name', 'Blog') }}</title>
 
 <!-- Favicon-->
-<link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}" />
+<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
 <!-- Core theme CSS (includes Bootstrap)-->
 <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
